edb1512ac062cb6b09e44bb5098ef2c06dbc4447
pigozzif <pigozzife@gmail.com>
2022-05-24 18:22:44 -0400

    save neural weights to vxa

--- VoxcraftVXA.py
+++ VoxcraftVXA.py
@@ -6,12 +6,16 @@ import os
 Does not yet include signaling parameters
 '''
 
+
 class VXA:
-    
-    def __init__(self, HeapSize=0.1, EnableCilia=0, EnableExpansion=1, DtFrac=0.95, BondDampingZ=1, ColDampingZ=0.8, SlowDampingZ=0.01,
-                EnableCollision=0, SimTime=5, TempPeriod=0.1, GravEnabled=1, GravAcc=-9.81, FloorEnabled=1, Lattice_Dim=0.01,
-                RecordStepSize=100, RecordVoxel=1, RecordLink=0, RecordFixedVoxels=1, VaryTempEnabled=1, TempAmplitude=20, TempBase=25,
-                TempEnabled=1):
+
+    def __init__(self, HeapSize=0.1, EnableCilia=0, EnableExpansion=1, DtFrac=0.95, BondDampingZ=1, ColDampingZ=0.8,
+                 SlowDampingZ=0.01,
+                 EnableCollision=0, SimTime=5, TempPeriod=0.1, GravEnabled=1, GravAcc=-9.81, FloorEnabled=1,
+                 Lattice_Dim=0.01,
+                 RecordStepSize=100, RecordVoxel=1, RecordLink=0, RecordFixedVoxels=1, VaryTempEnabled=1,
+                 TempAmplitude=20, TempBase=25,
+                 TempEnabled=1, NeuralWeights=None):
 
         root = etree.XML("<VXA></VXA>")
         root.set('Version', '1.1')
@@ -39,8 +43,12 @@ class VXA:
         self.TempAmplitude = TempAmplitude
         self.TempBase = TempBase
         self.TempEnabled = TempEnabled
-        
-        self.NextMaterialID = 1 # Material ID's start at 1, 0 denotes empty space
+        if NeuralWeights is not None:
+            self.NeuralWeights = ",".join([str(w) for w in NeuralWeights])
+        else:
+            self.NeuralWeights = ""
+
+        self.NextMaterialID = 1  # Material ID's start at 1, 0 denotes empty space
 
         self.set_default_tags()
 
@@ -50,11 +58,12 @@ class VXA:
         # GPU
         gpu = etree.SubElement(root, 'GPU')
         etree.SubElement(gpu, "HeapSize").text = str(self.HeapSize)
-        
+
         # Simulator
         simulator = etree.SubElement(root, "Simulator")
         etree.SubElement(simulator, "EnableCilia").text = str(self.EnableCilia)
-        etree.SubElement(simulator, "EnableExpansion").text = str(self.EnableExpansion) # 0 only contraction, 1 is contration + expansion
+        etree.SubElement(simulator, "EnableExpansion").text = str(
+            self.EnableExpansion)  # 0 only contraction, 1 is contration + expansion
 
         integration = etree.SubElement(simulator, "Integration")
         etree.SubElement(integration, "DtFrac").text = str(self.DtFrac)
@@ -73,7 +82,7 @@ class VXA:
         etree.SubElement(sub, "mtVAR").text = 't'
         etree.SubElement(sub, "mtCONST").text = str(self.SimTime)
 
-        fitness = etree.SubElement(simulator, "FitnessFunction") # default - maximum x distance
+        fitness = etree.SubElement(simulator, "FitnessFunction")  # default - maximum x distance
         add = etree.SubElement(fitness, "mtADD")
         mul = etree.SubElement(add, 'mtMUL')
         etree.SubElement(mul, "mtVAR").text = 'x'
@@ -82,13 +91,13 @@ class VXA:
         etree.SubElement(mul2, "mtVAR").text = 'y'
         etree.SubElement(mul2, "mtVAR").text = 'y'
 
-
         history = etree.SubElement(simulator, "RecordHistory")
-        etree.SubElement(history, "RecordStepSize").text = str(self.RecordStepSize) #Capture image every 100 time steps
-        etree.SubElement(history, "RecordVoxel").text = str(self.RecordVoxel) # Add voxels to the visualization
-        etree.SubElement(history, "RecordLink").text = str(self.RecordLink) # Add links to the visualization
-        etree.SubElement(history, "RecordFixedVoxels").text = str(self.RecordFixedVoxels) 
-        
+        etree.SubElement(history, "RecordStepSize").text = str(
+            self.RecordStepSize)  # Capture image every 100 time steps
+        etree.SubElement(history, "RecordVoxel").text = str(self.RecordVoxel)  # Add voxels to the visualization
+        etree.SubElement(history, "RecordLink").text = str(self.RecordLink)  # Add links to the visualization
+        etree.SubElement(history, "RecordFixedVoxels").text = str(self.RecordFixedVoxels)
+
         # Environment
 
         environment = etree.SubElement(root, "Environment")
@@ -98,6 +107,9 @@ class VXA:
         etree.SubElement(thermal, "TempPeriod").text = str(self.TempPeriod)
         etree.SubElement(thermal, "TempAmplitude").text = str(self.TempAmplitude)
         etree.SubElement(thermal, "TempBase").text = str(self.TempBase)
+
+        neural = etree.SubElement(environment, "Neural")
+        etree.SubElement(neural, "NeuralWeights").text = self.NeuralWeights
 
         gravity = etree.SubElement(environment, "Gravity")
         etree.SubElement(gravity, "GravEnabled").text = str(self.GravEnabled)
@@ -127,25 +139,25 @@ class VXA:
         etree.SubElement(data, "Layer").text = etree.CDATA("1")
 
     def add_material(self, E=10000, RHO=1000, P=0.35, CTE=0, uStatic=1, uDynamic=0.8,
-                      isSticky=0, hasCilia=0, isBreakable=0, isMeasured=1,
-                      RGBA=None, isFixed=0, TempPhase=0):
+                     isSticky=0, hasCilia=0, isBreakable=0, isMeasured=1,
+                     RGBA=None, isFixed=0, TempPhase=0):
 
         material_ID = self.NextMaterialID
-        self.NextMaterialID+=1
+        self.NextMaterialID += 1
 
         if RGBA is None:
-        # assign the material a random color
+            # assign the material a random color
             RGBA = np.around((np.random.random(), np.random.random(), np.random.random(), 1), 2)
         else:
-            if len(RGBA)==3: # if no alpha, add alpha of 255
-                RGBA = (RGBA[0],RGBA[1],RGBA[2],255)
-            
+            if len(RGBA) == 3:  # if no alpha, add alpha of 255
+                RGBA = (RGBA[0], RGBA[1], RGBA[2], 255)
+
             # normalize between 0-1
-            RGBA = (RGBA[0]/255,RGBA[1]/255,RGBA[2]/255,RGBA[3]/255)
+            RGBA = (RGBA[0] / 255, RGBA[1] / 255, RGBA[2] / 255, RGBA[3] / 255)
 
         palette = self.tree.find("*/Palette")
         material = etree.SubElement(palette, "Material")
-        
+
         etree.SubElement(material, "Name").text = str(material_ID)
 
         display = etree.SubElement(material, "Display")
@@ -155,13 +167,14 @@ class VXA:
         etree.SubElement(display, "Alpha").text = str(RGBA[3])
 
         mechanical = etree.SubElement(material, "Mechanical")
-        etree.SubElement(mechanical, "isMeasured").text = str(isMeasured) # if material should be included in fitness function
+        etree.SubElement(mechanical, "isMeasured").text = str(
+            isMeasured)  # if material should be included in fitness function
         etree.SubElement(mechanical, "Fixed").text = str(isFixed)
         etree.SubElement(mechanical, "sticky").text = str(isSticky)
         etree.SubElement(mechanical, "Cilia").text = str(hasCilia)
-        etree.SubElement(mechanical, "MatModel").text = str(isBreakable) # 0 = no failing
+        etree.SubElement(mechanical, "MatModel").text = str(isBreakable)  # 0 = no failing
         etree.SubElement(mechanical, "Elastic_Mod").text = str(E)
-        etree.SubElement(mechanical, "Fail_Stress").text = "0" # no fail if matModel is 0
+        etree.SubElement(mechanical, "Fail_Stress").text = "0"  # no fail if matModel is 0
         etree.SubElement(mechanical, "Density").text = str(RHO)
         etree.SubElement(mechanical, "Poissons_Ratio").text = str(P)
         etree.SubElement(mechanical, "CTE").text = str(CTE)
@@ -174,9 +187,9 @@ class VXA:
     def write(self, filename='base.vxa'):
 
         # If no material has been added, add default material
-        if self.NextMaterialID==0:
+        if self.NextMaterialID == 0:
             self.add_material()
-        
+
         with open(filename, 'w+') as f:
             f.write(etree.tostring(self.tree, encoding="unicode", pretty_print=True))
 
--- firstSimulation.py
+++ firstSimulation.py
@@ -5,7 +5,7 @@ from VoxcraftVXD import VXD
 
 # Generate a Base VXA file
 # See here for list of vxa tags: https://gpuvoxels.readthedocs.io/en/docs/
-vxa = VXA(EnableExpansion=1, SimTime=5) # pass vxa tags in here
+vxa = VXA(EnableExpansion=1, SimTime=5, NeuralWeights=[1,2,3,4,5]) # pass vxa tags in here
 
 # Create two materials with different properties
 mat1 = vxa.add_material(RGBA=(255,0,255), E=5e4, RHO=1e4) # returns the material ID
