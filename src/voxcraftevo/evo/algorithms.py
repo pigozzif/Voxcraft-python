2c233d84d17768d8292354d0ec02125e480e93d5
pigozzif <pigozzife@gmail.com>
2022-06-27 16:39:16 -0400

    fix bug

--- src/voxcraftevo/evo/algorithms.py
+++ src/voxcraftevo/evo/algorithms.py
@@ -63,7 +63,7 @@ class Solver(object):
             pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     def save_best(self, best: Individual) -> None:
-        if self.best_so_far is not None and self.best_so_far.id == best.id:
+        if self.best_so_far is None or self.best_so_far.id == best.id:
             return
         sub.call("rm {}/*".format(self.hist_dir), shell=True)
         self.fitness_func.save_histories(best=best, input_directory=self.data_dir, output_directory=self.hist_dir)
