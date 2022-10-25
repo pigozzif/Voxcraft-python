466e2399014490b5b2e714284ef882d67dacebce
pigozzif <pigozzife@gmail.com>
2022-10-25 17:51:49 -0400

    fix bug

--- src/voxcraftevo/evo/algorithms.py
+++ src/voxcraftevo/evo/algorithms.py
@@ -46,7 +46,7 @@ class Solver(object):
         if not os.path.isdir(executables_dir):
             sub.call("mkdir {}".format(executables_dir), shell=True)
         for file in os.listdir("../logs"):
-            if int(file.split(".")[1].split("_")[1]) == self.seed:
+            if int(file.split(".")[1].split("_")[1]) == self.seed and "out" in file:
                 self.log_file = "../logs/" + file
                 break
         else:
--- src/voxcraftevo/fitness/evaluation.py
+++ src/voxcraftevo/fitness/evaluation.py
@@ -17,7 +17,6 @@ class FitnessFunction(object):
 
     @staticmethod
     def parse_fitness_from_history(root, bot_id: str, fitness_tag: str) -> float:
-        print(root)
         with open(root, "r") as file:
             for line in file:
                 if line.startswith("-".join([bot_id, fitness_tag])):
