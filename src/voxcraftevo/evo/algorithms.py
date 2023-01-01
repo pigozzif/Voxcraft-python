27607b525eb35296aac858597bc60e6edffe69f6
pigozzif <pigozzife@gmail.com>
2023-01-01 19:32:05 +0100

    fix bug

--- src/voxcraftevo/evo/algorithms.py
+++ src/voxcraftevo/evo/algorithms.py
@@ -1,10 +1,12 @@
 import abc
 import time
-from typing import Dict
+from dataclasses import dataclass
+from typing import Dict, List
 
 import numpy as np
 import matplotlib.pyplot as plt
 import subprocess as sub
+from sklearn.cluster import KMeans
 
 from .operators.operator import GeneticOperator
 from .optimizers import Adam
@@ -185,6 +187,72 @@ class EvolutionaryStrategy(EvolutionarySolver):
         noise = np.array([(x.genotype - self.mode) / self.sigma for x in self.pop])
         fitness = np.array([x.fitness["fitness_score"] for x in self.pop])
         self.best_fitness = max(self.best_fitness, np.max(fitness))
+        theta_grad = (1.0 / (self.pop_size * self.sigma)) * np.dot(noise.T, fitness)
+        self.mode = self.optimizer.optimize(mean=self.mode, t=self.pop.gen, theta_grad=theta_grad)
+
+    def exp_decay(self):
+        self.sigma = self.sigma * self.sigma_decay
+        self.sigma = max(self.sigma, self.sigma_limit)
+
+    def evolve(self) -> None:
+        for child_genotype in self.build_offspring():
+            self.pop.add_individual(genotype=child_genotype)
+        self.evaluate_individuals()
+        self.update_mode()
+        self.pop.clear()
+        self.sigma = exp_decay(self.sigma, self.sigma_decay, self.sigma_limit)
+
+    def get_best_fitness(self) -> float:
+        return self.best_fitness
+
+
+@dataclass
+class SubPopulation:
+    id: int
+    n: int
+    mean: np.ndarray
+    members: List[Individual]
+    best_fitness: float
+
+
+class KMeansStrategy(EvolutionarySolver):
+
+    def __init__(self, seed, pop_size, genotype_factory, solution_mapper, sigma: float, sigma_decay: float,
+                 sigma_limit: float, num_dims: int, l_rate_init: float, l_rate_decay: float, l_rate_limit: float,
+                 num_modes: int, fitness_func, data_dir, hist_dir, pickle_dir, output_dir, executables_dir, logs_dir,
+                 listener, **kwargs):
+        super().__init__(seed=seed, pop_size=pop_size, genotype_factory=genotype_factory,
+                         solution_mapper=solution_mapper, fitness_func=fitness_func, remap=False,
+                         genetic_operators={}, data_dir=data_dir, hist_dir=hist_dir,
+                         pickle_dir=pickle_dir, output_dir=output_dir, executables_dir=executables_dir,
+                         logs_dir=logs_dir, listener=listener, comparator="lexicase", **kwargs)
+        self.survival_selector = Selector.create_selector(name="worst", **kwargs)
+        self.sigma = sigma
+        self.sigma_decay = sigma_decay
+        self.sigma_limit = sigma_limit
+        self.num_dims = num_dims
+        self.optimizers = [Adam(num_dims=num_dims, l_rate_init=l_rate_init, l_rate_decay=l_rate_decay,
+                           l_rate_limit=l_rate_limit) for _ in range(num_modes)]
+        self.pop.clear()
+        self.num_modes = num_modes
+        self.clustering = KMeans(n_clusters=self.num_modes, random_state=seed).fit(np.array([ind.genotype
+                                                                                             for ind in self.pop]))
+        self.sub_pops = [SubPopulation(id=i, mean=self.clustering.cluster_centers_[i],
+                                       n=self.pop_size // self.num_modes,
+                                       members=list(filter(lambda x: x == i, self.clustering.labels_)),
+                                       best_fitness=float("inf")) for i in range(self.num_modes)]
+
+    def build_offspring(self) -> list:
+        for sub_pop in self.sub_pops:
+            z_plus = np.random.normal(loc=0.0, scale=self.sigma, size=(self.pop_size // sub_pop.n, self.num_dims))
+            z = np.concatenate([z_plus, -1.0 * z_plus])
+
+        return [self.mode + x * self.sigma for x in z]
+
+    def update_mode(self) -> None:
+        noise = np.array([(x.genotype - self.mode) / self.sigma for x in self.pop])
+        fitness = np.array([x.fitness["fitness_score"] for x in self.pop])
+        self.best_fitness = min(self.best_fitness, np.min(fitness))
         theta_grad = (1.0 / (self.pop_size * self.sigma)) * np.dot(noise.T, fitness)
         self.mode = self.optimizer.optimize(mean=self.mode, t=self.pop.gen, theta_grad=theta_grad)
 
