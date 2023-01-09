5fe7517bd04e0642d8e5dd5f7b28079c9d11d163
pigozzif <pigozzife@gmail.com>
2023-01-09 12:47:38 +0100

    fix typo

--- src/main.py
+++ src/main.py
@@ -148,12 +148,12 @@ if __name__ == "__main__":
     else:
         n_modes = len(targets) * 2
     if number_of_params == 2:
-        listener = VizListener(file_path=".".join([str(arguments.clustering), str(seed), str(arguments.n_clusters),
+        listener = VizListener(file_path=".".join([str(arguments.clustering), str(seed), str(arguments.num_clusters),
                                                    str(arguments.num_dims), str(arguments.num_targets), "txt"]),
                                header=["iteration", "elapsed.time", "best.fitness", "avg.distance", "distances"],
                                targets=targets)
     else:
-        listener = MyListener(file_path=".".join([str(arguments.clustering), str(seed), str(arguments.n_clusters),
+        listener = MyListener(file_path=".".join([str(arguments.clustering), str(seed), str(arguments.num_clusters),
                                                   str(arguments.num_dims), str(arguments.num_targets), "txt"]),
                               header=["iteration", "elapsed.time", "best.fitness", "avg.distance", "distances"],
                               targets=targets)
