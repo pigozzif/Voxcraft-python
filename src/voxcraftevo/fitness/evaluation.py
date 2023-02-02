5c473622053d885dba28988474debb34c3a77f08
pigozzif <pigozzife@gmail.com>
2023-02-02 20:44:27 +0100

    debug

--- src/voxcraftevo/fitness/evaluation.py
+++ src/voxcraftevo/fitness/evaluation.py
@@ -14,7 +14,7 @@ class FitnessFunction(object):
         detail = root.findall("detail/")
         for d in detail:
             if d.tag == bot_id:
-                print(d)
+                print(d.findall(fitness_tag))
                 return float(d.findall(fitness_tag)[0].text)
         return worst_value
 
