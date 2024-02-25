8bddcdb6836a7d6064d9167d6a894bfcb193cc94
pigozzif <pigozzife@gmail.com>
2024-02-25 19:01:26 +0100

    clean up

--- src/clean_git.py
+++
@@ -1,6 +0,0 @@
-import os
-
-for root, dirs, files in os.walk("."):
-    for file in files:
-        if file.endswith(".avi") or file.endswith(".pickle"):
-            os.system("git rm --cached {}".format(os.path.join(root, file)))
